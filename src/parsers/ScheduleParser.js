5542dc7cdf245e8b6cc2e2848f0631ee02bfb2be
kbaumgart <keith.baumgart@gmail.com>
2017-10-11 21:02:07 -0400

    Update ScheduleParser.js

--- src/parsers/ScheduleParser.js
+++ src/parsers/ScheduleParser.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const _ = require('lodash');
 
-axios.get('http://data.nba.net/data/10s/prod/v1/2016/schedule.json').then(res => {
+axios.get('http://data.nba.net/data/10s/prod/v1/2017/schedule.json').then(res => {
   const data = {};
   _.each(res.data.league.standard, game => {
     data[game.gameId] = {
